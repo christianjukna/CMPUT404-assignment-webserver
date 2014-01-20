190b905500c49267a236697b2ef80d07c3799ed6
christianjukna <jukna@ualberta.ca>
2014-01-19 18:18:04 -0700

    HTTP1.1 and screenshots

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import SocketServer
+import os
 # coding: utf-8
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
@@ -17,7 +18,7 @@ import SocketServer
 #
 #
 # Furthermore it is derived from the Python documentation examples thus
-# some of the code is Copyright © 2001-2013 Python Software
+# some of the code is Copyright 2001-2013 Python Software
 # Foundation; All Rights Reserved
 #
 # http://docs.python.org/2/library/socketserver.html
@@ -32,8 +33,54 @@ class MyWebServer(SocketServer.BaseRequestHandler):
     def handle(self):
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
-        self.request.sendall("OK")
+        
+        # Split the request for parsing
+        # 0 - Request
+        # 1 - File Path
+        requestData = self.data.split()
+        baseUrl = '/www'
 
+        # Check if the filepath leads out of the www directory
+        if "../" in requestData[1]:
+            error = ("HTTP/1.1 404 Not Found\n")
+            self.request.sendall(error)
+            return  
+        
+        filePath = self.append_index(os.getcwd() + baseUrl + requestData[1])
+        # Make Sure is is a GET command
+        if "GET" != requestData[0]: 
+            error = ("HTTP/1.1 501 Not Implemented\n")
+            self.request.sendall(error)
+            return         
+
+        # Handle all the possible requests and respond according to HTTP
+        # error codes
+        print filePath
+        if os.path.isfile(filePath) and "../" not in filePath :  
+            response = ("HTTP/1.1 200 OK\n" +
+            "Content-Type: "+self.html_or_css(filePath)+"\n\n"
+            + open(filePath,"r").read() +"\n")
+            self.request.sendall(response)
+            return
+        else:
+            error = ("HTTP/1.1 404 Not Found\n")
+            self.request.sendall(error)
+            return  
+        
+    def html_or_css(self,fileRequest):
+        fileType = os.path.splitext(fileRequest)[1]
+        if (fileType == ".css"):
+            return "text/css"
+        if fileType == ".html":
+            return "text/html"
+        
+    def append_index(self,fileRequest):
+        fileRequest = os.path.realpath(fileRequest)
+        if (os.path.isdir(fileRequest)):
+            return fileRequest + "/index.html"
+        else:
+            return fileRequest
+            
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
 
